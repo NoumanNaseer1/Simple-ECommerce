31e9b5b2a8ca051f1d9378515fef4f5042633608
Nouman <bcs.12.41@gmail.com>
2021-03-27 19:42:34 +0100

    First Commit

---
+++ src/Item/Item.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Button from "@material-ui/core/Button";
+//Styles
+import { Wrapper } from "./Item.styles";
+
+const Item = ({ item, handleAddToCart }) => {
+  return (
+    <Wrapper>
+      <img src={item.image} alt={item.title} />
+      <div>
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+        <h3>${item.price}</h3>
+      </div>
+      <Button onClick={() => handleAddToCart(item)}>Add to cart</Button>
+    </Wrapper>
+  );
+};
+export default Item;
